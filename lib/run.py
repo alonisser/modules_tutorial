9c6a88f4b56c8cde8792400567b70a9223809662
alonisser <alonisser@gmail.com>
2020-08-26 10:46:49 +0300

    emoji

--- lib/run.py
+++ lib/run.py
@@ -1,5 +1,6 @@
 from helpers.text import upper_name, fancy_pad, lowly_pad
 from logic import is_too_long
+import emoji
 
 if __name__ == "__main__":
     name = upper_name('dror')
@@ -9,3 +10,5 @@ if __name__ == "__main__":
     print(final_text)
 
     print(f'is too long: {is_too_long(final_text)}')
+
+    print(emoji.emojize(f'give a {name} :red_heart:'))
