ee6ad873b612d540ad2b51d1a1f3c83141b1bf98
alonisser <alonisser@gmail.com>
2020-08-26 10:16:26 +0300

    add function

--- lib/run.py
+++ lib/run.py
@@ -2,6 +2,11 @@ def upper_name(a_name):
     return a_name.upper()
 
 
+def fancy_pad(text):
+    return f'**{text}**'
+
+
 if __name__ == "__main__":
     name = upper_name('dror')
-    print(name)
+
+    print(fancy_pad(name))
