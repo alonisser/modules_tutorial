abd905a717574f62c9792c0b14f0159002d540b6
alonisser <alonisser@gmail.com>
2020-08-26 10:34:37 +0300

    module as file

---
+++ lib/logic.py
@@ -0,0 +1,5 @@
+MAX_THRESHOLD = 13
+
+
+def is_too_long(text):
+    return len(text) > MAX_THRESHOLD
--- lib/run.py
+++ lib/run.py
@@ -1,7 +1,11 @@
 from helpers.text import upper_name, fancy_pad, lowly_pad
+from logic import is_too_long
 
 if __name__ == "__main__":
     name = upper_name('dror')
 
-    pad = fancy_pad(name)
-    print(lowly_pad(pad))
+    padded_text = fancy_pad(name)
+    final_text = lowly_pad(padded_text)
+    print(final_text)
+
+    print(f'is too long: {is_too_long(final_text)}')
